0be45ef5e1358e9c6658165ade06ace7b38daf2f
Isutomu <isutomu@gmail.com>
2025-02-23 12:56:49 -0300

    :bug: fix: Adjust url for fetching on sign up

--- src/utils/backend/fetchSignUp.jsx
+++ src/utils/backend/fetchSignUp.jsx
@@ -1,5 +1,5 @@
 export default async function fetchSignUp(userInfo) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/signUp`, {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/signup`, {
     mode: "cors",
     method: "POST",
     body: userInfo,
